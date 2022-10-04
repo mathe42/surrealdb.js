2a640b94e610432e1b3c77a0683757b19dfdf6e7
mathe42 <2pi_r2@gmx.de>
2022-10-04 10:12:49 +0200

    optimize once in emitter by using seperate object

--- src/classes/emitter.ts
+++ src/classes/emitter.ts
@@ -17,6 +17,9 @@ export class Emitter<EVENTS extends EventMap = EventMap> {
 	#events: {
 		[K in keyof EVENTS]?: Set<(this: this, ...args: EVENTS[K]) => void>;
 	} = {};
+	#eventsOnce: {
+		[K in keyof EVENTS]?: Set<(this: this, ...args: EVENTS[K]) => void>;
+	} = {};
 
 	static once = once;
 
@@ -40,10 +43,16 @@ export class Emitter<EVENTS extends EventMap = EventMap> {
 		listener: (this: this, ...args: EVENTS[T]) => void,
 	) {
 		this.#events[eventName]?.delete(listener);
+		this.#eventsOnce[eventName]?.delete(listener);
 
 		if (this.#events[eventName]?.size === 0) {
 			delete this.#events[eventName];
 		}
+
+		if (this.#eventsOnce[eventName]?.size === 0) {
+			delete this.#eventsOnce[eventName];
+		}
+
 		return this;
 	}
 
@@ -51,10 +60,11 @@ export class Emitter<EVENTS extends EventMap = EventMap> {
 		eventName: T,
 		listener: (this: this, ...args: EVENTS[T]) => void,
 	) {
-		return this.on(eventName, function once(...args: EVENTS[T]) {
-			this.removeListener(eventName, once);
-			listener.apply(this, args);
-		});
+		if (!this.#eventsOnce[eventName]) {
+			this.#eventsOnce[eventName] = new Set();
+		}
+		this.#eventsOnce[eventName]!.add(listener);
+		return this;
 	}
 
 	emit<T extends keyof EVENTS>(eventName: T, ...args: EVENTS[T]) {
@@ -62,14 +72,22 @@ export class Emitter<EVENTS extends EventMap = EventMap> {
 			listener.apply(this, args)
 		);
 
+		this.#eventsOnce[eventName]?.forEach((listener) =>
+			listener.apply(this, args)
+		);
+
+		delete this.#eventsOnce[eventName];
+
 		return this;
 	}
 
 	removeAllListeners(eventName?: keyof EVENTS) {
 		if (eventName) {
 			delete this.#events[eventName];
+			delete this.#eventsOnce[eventName];
 		} else {
 			this.#events = {};
+			this.#eventsOnce = {};
 		}
 		return this;
 	}
